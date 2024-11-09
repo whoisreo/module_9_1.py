e842aefc2145e4489a3a8653535a3fcadfb475b3
whoisreo <maksim03999@gmail.com>
2024-11-09 14:39:11 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list:(int, float), *functions):
+    result = {}
+    for func in functions:
+        result[str(func.__name__)] = func(int_list)
+    return result
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
